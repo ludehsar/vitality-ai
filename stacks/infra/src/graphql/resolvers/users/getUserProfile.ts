39c35cc73c0c5fb2800036656b6d716122f8846f
Rashedul Alam <rashedulalam112112@gmail.com>
2025-06-04 12:24:47 +0600

    fix(infra): fixed issues for successful deployment

--- stacks/infra/src/graphql/resolvers/users/getUserProfile.ts
+++ stacks/infra/src/graphql/resolvers/users/getUserProfile.ts
@@ -1,11 +1,16 @@
-import { Context } from '@aws-appsync/utils'
-import { GetUserProfileQueryVariables, UserProfile } from '@vitality-ai/graphql'
-import * as ddb from '@aws-appsync/utils/dynamodb'
+import { Context } from '@aws-appsync/utils';
+import * as ddb from '@aws-appsync/utils/dynamodb';
+import {
+  GetUserProfileQueryVariables,
+  UserProfile,
+} from '@vitality-ai/graphql';
 
 export function request(ctx: Context<GetUserProfileQueryVariables>) {
-  return ddb.get({ key: { PK: `USER#${ctx.args.id}` } });
+  return ddb.get({
+    key: { PK: `USER#${ctx.args.id}`, SK: `USER#${ctx.args.id}` },
+  });
 }
 
 export function response(ctx: Context) {
-  return ctx.result.items as UserProfile | null;
+  return ctx.result as UserProfile;
 }
--- stacks/infra/src/stacks/app-stack.ts
+++ stacks/infra/src/stacks/app-stack.ts
@@ -1,8 +1,16 @@
-import { Construct } from 'constructs';
+import { schemaPath } from '@vitality-ai/graphql';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { BaseDynamoDBConstruct } from '../constructs/base-dynamodb-construct';
+import {
+  AuthorizationType,
+  Code,
+  FunctionRuntime,
+  SchemaFile,
+} from 'aws-cdk-lib/aws-appsync';
 import { AttributeType } from 'aws-cdk-lib/aws-dynamodb';
-import { ApiStack } from './api-stack';
+import { Construct } from 'constructs';
+import path from 'path';
+import { BaseDynamoDBConstruct } from '../constructs/base-dynamodb-construct';
+import { GraphqlApiConstruct } from '../constructs/graphql-construct';
 
 export interface AppStackProps extends StackProps {
   readonly stageName: string;
@@ -12,46 +20,73 @@ export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props: AppStackProps) {
     super(scope, id, props);
 
+    const graphqlApi = new GraphqlApiConstruct(this, 'VitalityAIGraphqlApi', {
+      name: 'vitalityai-graphql-api',
+      stageName: props.stageName,
+      definition: {
+        schema: SchemaFile.fromAsset(schemaPath),
+      },
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+        },
+      },
+    });
+
     const table = new BaseDynamoDBConstruct(this, 'VitalityAISingleTable', {
       stageName: props.stageName,
       partitionKey: {
         name: 'PK',
-        type: AttributeType.STRING
+        type: AttributeType.STRING,
       },
       sortKey: {
         name: 'SK',
-        type: AttributeType.STRING
+        type: AttributeType.STRING,
       },
-      globalSecondaryIndexes:
-        [
-          {
-            indexName: 'GSI1',
-            partitionKey: {
-              name: 'GSI1PK',
-              type: AttributeType.STRING
-            },
-            sortKey: {
-              name: 'GSI1SK',
-              type: AttributeType.STRING
-            }
+      globalSecondaryIndexes: [
+        {
+          indexName: 'GSI1',
+          partitionKey: {
+            name: 'GSI1PK',
+            type: AttributeType.STRING,
           },
-          {
-            indexName: 'GSI2',
-            partitionKey: {
-              name: 'GSI2PK',
-              type: AttributeType.STRING
-            },
-            sortKey: {
-              name: 'GSI2SK',
-              type: AttributeType.STRING
-            }
-          }
-        ],
+          sortKey: {
+            name: 'GSI1SK',
+            type: AttributeType.STRING,
+          },
+        },
+        {
+          indexName: 'GSI2',
+          partitionKey: {
+            name: 'GSI2PK',
+            type: AttributeType.STRING,
+          },
+          sortKey: {
+            name: 'GSI2SK',
+            type: AttributeType.STRING,
+          },
+        },
+      ],
     });
 
-    new ApiStack(this, 'ApiStack', {
-      stageName: props.stageName,
-      table,
+    const ddbDataSource = graphqlApi.addDynamoDbDataSource(
+      'VitalityAISingleTableDDBDataSource',
+      table
+    );
+
+    ddbDataSource.createResolver('getUserProfile', {
+      typeName: 'Query',
+      fieldName: 'getUserProfile',
+      runtime: FunctionRuntime.JS_1_0_0,
+      code: Code.fromAsset(
+        path.join(
+          __dirname,
+          '..',
+          'graphql',
+          'resolvers-compiled',
+          'getUserProfile.js'
+        )
+      ),
     });
   }
 }
