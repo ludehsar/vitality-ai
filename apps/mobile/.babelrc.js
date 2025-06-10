7bbd9133952bcf251ee34fcf36b1e2d36b2c9a8d
Rashedul Alam <rashedulalam112112@gmail.com>
2025-06-11 02:14:37 +0600

    fix(mobile): fix gluestack ui styles not visible issue

--- apps/mobile/.babelrc.js
+++ apps/mobile/.babelrc.js
@@ -13,6 +13,7 @@ module.exports = function (api) {
             runtime: 'automatic',
           },
         ],
+        'nativewind/babel',
       ],
     };
   }
--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,13 +1,12 @@
+import './styles/global.css';
+
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react-native';
 import { createStaticNavigation } from '@react-navigation/native';
 import { RootStack } from './navigations/RootStack';
 import { GluestackUIProvider } from './components/ui/gluestack-ui-provider';
 
-import './styles/global.css';
-
 import outputs from '../amplify_outputs.json';
-import { Button, ButtonGroup, ButtonText } from './components/ui/button';
 
 Amplify.configure(outputs);
 
@@ -15,21 +14,13 @@ const Navigation = createStaticNavigation(RootStack);
 
 export const App = () => {
   return (
-    <Authenticator.Provider>
-      <Authenticator
-        Footer={() => (
-          <ButtonGroup>
-            <Button variant="solid" action="primary">
-              <ButtonText>Sign Out</ButtonText>
-            </Button>
-          </ButtonGroup>
-        )}
-      >
-        <GluestackUIProvider>
+    <GluestackUIProvider>
+      <Authenticator.Provider>
+        <Authenticator>
           <Navigation />
-        </GluestackUIProvider>
-      </Authenticator>
-    </Authenticator.Provider>
+        </Authenticator>
+      </Authenticator.Provider>
+    </GluestackUIProvider>
   );
 };
 
