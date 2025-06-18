be704f6773507d5e334e1a63cbc238fbef0670a4
Rashedul Alam <rashedulalam112112@gmail.com>
2025-06-18 18:50:08 +0600

    chore(mobile): move to src directory

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -3,8 +3,8 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
-import '@/global.css';
-import { GluestackUIProvider } from '../components/ui/gluestack-ui-provider';
+import '@/styles/global.css';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
@@ -12,12 +12,12 @@ import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '../hooks/useColorScheme';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   if (!loaded) {
